454f0a0df39c0d5f8254b07a73e9d41ca0ad4144
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-01 13:54:00 +0300

    Homework_Ignat week_1 done

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import classes from './Message.module.css';
+
+type MessagePropsType = {
+  avatar: string
+  name: string
+  message: string
+  time: string
+}
+
+function Message(props: MessagePropsType) {
+  return (
+    <div className={classes.message}>
+      <div className={classes.imageContainer}>
+        <img src={props.avatar} alt="User avatar"/>
+      </div>
+      <div className={classes.messageCloud}>
+        <div
+          className={classes.userName}>{props.name}</div>
+        <div
+          className={classes.userMessage}>{props.message}</div>
+        <div
+          className={classes.timeStamp}>{props.time}</div>
+      </div>
+    </div>
+  )
+}
+
+export default Message
